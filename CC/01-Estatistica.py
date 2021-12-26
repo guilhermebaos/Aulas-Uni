98ce218d44ff9f5e7a9b8db8839dda4526d2c83e
Guilherme Baos <guilhermebaos@gmail.com>
2021-12-26 16:38:04 +0000

    Aula CC

--- CC/01-Estatistica.py
+++ CC/01-Estatistica.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 massas = np.array([100.1, 101.5, 100.4, 100.7, 101.2, 102.1])
-
+N = len(massas)
 
 media = np.mean(massas)
 desvios = np.array([x - media for x in massas])
@@ -13,4 +13,7 @@ print(np.mean(desvios))
 print(np.mean(desvios_abs))
 print(np.var(massas))
 print(np.std(massas))
-print(np.std(massas) / (len(massas) ** 0.5))
+print(np.std(massas) / (N ** 0.5))
+print(1/((2 * (N - 1))**0.5))
+
+
