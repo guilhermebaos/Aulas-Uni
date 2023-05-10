955c40d2169ea2114f198284298a6a46a4130c45
Guilherme Silva <guilhermebaos@gmail.com>
2023-05-10 16:48:27 +0100

    Ficheiro com constantes úteis

---
+++ physics.py
@@ -0,0 +1,34 @@
+# Ficheiro de Python com constantes físicas que dão jeito em unidades SI
+
+# Constante de Planck
+h = 6.626e-34
+
+# Carga elementar
+q = 1.602e-19
+
+# Constante gravitacional
+G = 6.674e-11
+
+# Número de avogrado
+N = 6.022e23
+
+# Massa do eletrão
+me = 9.109e-31
+
+# Massa do protão
+mp = 1.673e-27
+
+# Permitividade do vácuo
+eps0 = 8.854e-12
+
+# Permeabilidade do vácuo
+mu0 = 1.257e-6
+
+# Constante de Boltzmann
+kb = 1.381e-23
+
+# Uma atmosfera em Pascal
+atm = 101325
+
+# Constante de Wien
+B = 2.898e-3
