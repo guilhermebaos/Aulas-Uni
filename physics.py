637ac104901e3827a55c51ff3d8b8c9b1285aaf6
Guilherme Silva <guilhermebaos@gmail.com>
2023-06-09 15:44:01 +0100

    [FC] Terminar Ficha 5

--- physics.py
+++ physics.py
@@ -1,37 +1,39 @@
 # Ficheiro de Python com constantes físicas que dão jeito, em unidades SI
+import numpy as np
 
 # Velocidade da Luz
-c = 299792458
+c = 299_792_458
 
 # Constante de Planck
-h = 6.626e-34
+h = 6.626_068e-34
+hbar = h / (2 * np.pi)
 
 # Carga elementar
-q = 1.602e-19
+q = 1.602_176_46e-19
 
 # Constante gravitacional
-G = 6.674e-11
+G = 6.674_30e-11
 
 # Número de avogrado
-N = 6.022e23
+N = 6.022_140_76e23
 
 # Massa do eletrão
-me = 9.109e-31
+me = 9.109_381_88e-31
 
 # Massa do protão
-mp = 1.673e-27
+mp = 1.672_621_58e-27
 
 # Permitividade do vácuo
-eps0 = 8.854e-12
+eps0 = 8.854_187_8e-12
 
 # Permeabilidade do vácuo
-mu0 = 1.257e-6
+mu0 = 1.256_637e-6
 
 # Constante de Boltzmann
-kb = 1.381e-23
+kb = 1.380_650_3e-23
 
 # Uma atmosfera em Pascal
-atm = 101325
+atm = 101_325
 
 # Constante de Wien
-B = 2.898e-3
+B = 2.897_771_955e-3
